8bf68c693bbbec4ea69ad108b52dc9f3da56424e
keesiemeijer <keesie.meijer@gmail.com>
2014-10-04 13:23:36 +0200

    Added function for line numbers: browser_supports_animation() New click event listener for the line numbers link

--- public/js/wp-template-viewer.js
+++ public/js/wp-template-viewer.js
@@ -1,7 +1,11 @@
 ( function( $ ) {
 
-	var wp_tv_path;
 	var code_container = $( '<div style="display:none"></div>' );
+	var animation_elm = document.createElement( 'div' );
+	var in_progress = false;
+	var linenumbers = false;
+	var linesNum;
+
 
 	// function to scroll to element
 	function wp_tv_scroll_to( el ) {
@@ -16,6 +20,7 @@
 			} );
 		}
 	}
+
 
 	// function to select content
 	function wp_tv_select_text( element ) {
@@ -34,98 +39,203 @@
 	}
 
 
+	// checks browser support for css animations
+	function browser_supports_animation() {
+
+		var animation = false,
+			elm = animation_elm,
+			domPrefixes = 'Webkit Moz O ms Khtml'.split( ' ' );
+
+		// check unprefixed
+		if ( elm.style.animationName !== undefined ) {
+			animation = true;
+		}
+
+		if ( animation === false ) {
+			for ( var i = 0; i < domPrefixes.length; i++ ) {
+				if ( elm.style[ domPrefixes[ i ] + 'AnimationName' ] !== undefined ) {
+					console.log( 'prefix ' + domPrefixes[ i ] );
+					animation = true;
+					break;
+				}
+			}
+		}
+
+		return animation;
+	}
+
+
+	// adds line number spans to pre tag
+	function line_numbers( element ) {
+		var code_tag, lines, lineNumbersWrapper;
+		var code = $( 'code', element );
+
+		if ( code.length ) {
+			linesNum = ( 1 + code.text().split( '\n' ).length );
+		} else {
+			linesNum = ( 1 + element.text().split( '\n' ).length );
+		}
+
+		lines = new Array( linesNum );
+		lines = lines.join( '<span></span>' );
+
+		lineNumbersWrapper = $( '<span class="line-numbers-rows">' + lines + "</span>" );
+
+		element.prepend( lineNumbersWrapper );
+	}
+
+
 	$( document ).ready( function() {
 
 		// Change .wp_tv_no_js to .wp_tv_js (unhides links when there is Javascript)
 		$( '.wp_tv_no_js' ).toggleClass( 'wp_tv_no_js wp_tv_js' );
 
 		// Make links.
-		$( ".wp_tv_path" ).removeClass( "ab-item ab-empty-item" ).wrap( '<a class="ab-item wp_tv_file" href=""></a>' );
+		$( "[data-wp_tv_file]" ).removeClass( "ab-item ab-empty-item" ).wrap( '<a class="ab-item" href=""></a>' );
 		$( ".wp_tv_toggle" ).contents().unwrap().wrap( '<a class="wp_tv_toggle" href=""></a>' );
 		$( ".wp_tv_toggle" ).parent().removeClass( "ab-empty-item" );
 		$( ".wp_tv_close" ).contents().unwrap().wrap( '<a class="wp_tv_close" href=""></a>' );
 		$( ".wp_tv_close" ).attr( 'alt', wp_tv_ajax.wp_tv_close );
 		$( ".wp_tv_close" ).attr( 'title', wp_tv_ajax.wp_tv_close );
 
-		var file_list = $( '.wp_tv_files' );
+		var header = $( '.wp_tv_header' );
 		var toggle_admin_bar = $( "#wpadminbar" ).find( ".wp_tv_toggle" );
-		var toggle_footer = file_list.find( ".wp_tv_toggle" );
-		var ul = $( '#wp_tv_file_list_footer' );
+		var toggle_footer = header.find( ".wp_tv_toggle" );
+		var footer_files = $( '#wp_tv_template_viewer' ).find( '.wp_tv_files' );
 
 
 		// Append empty code container.
 		$( '#wp_tv_template_viewer' ).append( code_container );
 
-		// close link
+		// close click event
 		$( ".wp_tv_close" ).on( "click", function( event ) {
 			event.preventDefault();
-			ul.hide();
-			file_list.hide();
+			footer_files.hide();
+			header.hide();
 			toggle_admin_bar.text( wp_tv_ajax.wp_tv_show_in_footer );
 			toggle_footer.text( wp_tv_ajax.wp_tv_show );
 			code_container.hide();
 			code_container.empty();
 		} );
 
-		// toggle links
+		// show files click event
 		$( ".wp_tv_toggle" ).on( "click", function( event ) {
 			event.preventDefault();
 
-			if ( ul.is( ":visible" ) ) {
+			if ( footer_files.is( ":visible" ) ) {
 				toggle_admin_bar.text( wp_tv_ajax.wp_tv_show_in_footer );
 				toggle_footer.text( wp_tv_ajax.wp_tv_show );
-				ul.slideUp( "fast" );
+				footer_files.slideUp( "fast" );
 
 			} else {
 				toggle_admin_bar.text( wp_tv_ajax.wp_tv_hide_in_footer );
 				toggle_footer.text( wp_tv_ajax.wp_tv_hide );
-				file_list.show();
-				ul.show();
+				header.show();
+				footer_files.show();
 			}
-
-			wp_tv_scroll_to( file_list );
+			wp_tv_scroll_to( header );
 		} );
 
-		// select content link
+		// select text click event
 		$( '#wp_tv_template_viewer' ).on( 'click', '.wp_tv_select', function( event ) {
 			event.preventDefault();
-			wp_tv_scroll_to( $( "#wp_tv_code_title" ) );
+			wp_tv_scroll_to( $( "#wp_tv_file_title" ) );
 			wp_tv_select_text( 'wp_tv_content' );
 		} );
 
-
-		// file links
-		$( ".wp_tv_file" ).on( "click", function( event ) {
+		// show/hide line numbers click event
+		$( '#wp_tv_template_viewer' ).on( 'click', '.wp_tv_lines', function( event ) {
 			event.preventDefault();
 
-			// show title (and files) (todo: create close button)
-			file_list.show();
+			var pre = $( '#wp_tv_content' ).find( 'pre' );
+			linesNum = 0;
 
-			// Hide all the things.
-			code_container.hide();
-			code_container.empty();
+			if ( pre.length ) {
 
-			wp_tv_path = $( this ).children( 'span' ).data( 'wp_tv_path' );
+				if ( !pre.hasClass( 'line-numbers' ) ) {
+					linenumbers = true;
+					pre.addClass( 'line-numbers' );
+					line_numbers( pre );
+					$( this ).text( wp_tv_ajax.wp_tv_hide_lines );
 
-			$.post( wp_tv_ajax.wp_tv_ajaxurl, {
+				} else {
+					linenumbers = false;
+					pre.removeClass( 'line-numbers large' );
+					pre.find( '.line-numbers-rows' ).remove();
+					$( this ).text( wp_tv_ajax.wp_tv_lines );
+				}
+
+				if ( line_numbers && ( linesNum >= 1000 ) ) {
+					pre.addClass( 'large' );
+				}
+			}
+		} );
+
+
+		// file click event
+		$( ".wp_tv_files" ).find( 'a' ).on( "click", function( event ) {
+
+			event.preventDefault();
+
+			if ( in_progress ) {
+				return;
+			}
+
+			// let's get the file content for this request
+			in_progress = true;
+
+			// get the file path from link
+			var file = $( this ).children( 'span' ).data( 'wp_tv_file' );
+			var support = browser_supports_animation() ? ' cssanimations' : ' no-cssanimations';
+
+			// add  a spinner to links with the same data-wp_tv_path
+			$( '[data-wp_tv_file="' + file + '"]' ).each( function() {
+				if ( $.contains( footer_files.get( 0 ), this ) ) {
+					$( this ).parent().append( $( '<span class="wp_tv_spinner' + support + '"></span>' ) );
+				} else {
+					$( this ).parent().prepend( $( '<span class="wp_tv_spinner' + support + '"></span>' ) );
+				}
+			} );
+
+			// show header
+			header.show();
+
+			$.post(
+				wp_tv_ajax.wp_tv_ajaxurl, {
 					action: 'wp_tv_display_template_file',
 					wp_tv_nonce: wp_tv_ajax.wp_tv_nonce,
-					wp_tv_file: wp_tv_path
+					wp_tv_file: file
 				},
+
 				function( response ) {
+
+					// remove content from container
+					code_container.empty();
+
+					// show the container if it was hidden
 					code_container.show();
 
 					if ( response.success === true ) {
+						// show file content
 						code_container.html( response.data );
 					} else {
 						if ( response.data.length ) {
+							// show error
 							code_container.html( response.data );
 						}
 					}
-					//Prism.highlightElement(code_container.find('code')[0]);
-					//console.log(Prism);
-					wp_tv_scroll_to( $( '#wp_tv_code_title' ) );
-					
+
+					if ( linenumbers ) {
+						$( '.wp_tv_lines' ).trigger( "click" );
+					}
+
+					var title = $( '#wp_tv_file_title' );
+					wp_tv_scroll_to( title );
+
+					// remove spinner
+					$( '.wp_tv_spinner' ).remove();
+
+					in_progress = false;
 				}, "json" );
 
 		} );
